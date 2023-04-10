d5ec2bdd9976f4667dbb3ee94408b23d2031c84d
Piyush Prateek <piyushprateek231996@gmail.com>
2023-04-10 22:00:40 +0530

    new project created successfully

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import type { Application, Request, Response } from 'express';
+
+const app: Application = express();
+import { v4 } from 'uuid';
+
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello toto');
+});
+
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port} !`);
+});
+
+module.exports = app;
